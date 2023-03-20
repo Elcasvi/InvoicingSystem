696b6363480822a339580e2e3917499aaeecf808
Elcasvi <casvi.sanchez@gmail.com>
2023-03-19 20:05:44 -0600

    Initial committ of the project

---
+++ InvoicingSystemMVC/InvoicingSystemMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InvoicingSystemMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
