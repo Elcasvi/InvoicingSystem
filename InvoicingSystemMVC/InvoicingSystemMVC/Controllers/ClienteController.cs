f073402f08985753c38fe7a1fc2d27b5dd22aa0c
Elcasvi <casvi.sanchez@gmail.com>
2023-03-26 18:04:17 -0600

    Creo las vistas y los controladores de las paginas para crear Facturas y registrar Clientes

---
+++ InvoicingSystemMVC/InvoicingSystemMVC/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoicingSystemMVC.Controllers;
+
+public class ClienteController:Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public ClienteController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ InvoicingSystemMVC/InvoicingSystemMVC/Controllers/FacturaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoicingSystemMVC.Controllers;
+
+public class FacturaController:Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public FacturaController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
