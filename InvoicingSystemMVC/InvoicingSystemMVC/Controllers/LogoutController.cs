59414b47371dbc967fef8070871552debe597037
Elcasvi <casvi.sanchez@gmail.com>
2023-04-06 18:59:42 -0500

    Logro hacer el logout del contribuyente

--- InvoicingSystemMVC/InvoicingSystemMVC/Controllers/LogoutController.cs
+++ InvoicingSystemMVC/InvoicingSystemMVC/Controllers/LogoutController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using System.Net;
+using InvoicingSystemMVC.Models.Entities;
+using InvoicingSystemMVC.Models.Interfaces;
+using InvoicingSystemMVC.Models.ViewModels;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace InvoicingSystemMVC.Controllers;
 
 public class LogoutController:Controller
 {
-    public IActionResult Index()
+    private readonly IContribuyenteRepository _contribuyenteRepository;
+
+    public LogoutController(IContribuyenteRepository contribuyenteRepository)
     {
-        return View();
+        _contribuyenteRepository = contribuyenteRepository;
+    }
+
+    public async Task<IActionResult> Index(string RFC)
+    {
+        LogoutContribuyenteViewModel logoutContribuyenteVM = new LogoutContribuyenteViewModel()
+        {
+            Contribuyente = await _contribuyenteRepository.GetContribuyenteByRFC(RFC)
+        };
+        return View(logoutContribuyenteVM);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index()
+    {
+        string key = "RFC";
+        string value = string.Empty;
+        CookieOptions options = new CookieOptions()
+        {
+            Expires = DateTime.Now.AddDays(-1)
+        };
+        Response.Cookies.Append(key,value,options);
+        return RedirectToAction("Index","Home");
     }
 }
---
+++ InvoicingSystemMVC/InvoicingSystemMVC/Models/ViewModels/Contribuyentes/LogoutContribuyenteViewModel.cs
@@ -0,0 +1,8 @@
+﻿using InvoicingSystemMVC.Models.Entities;
+
+namespace InvoicingSystemMVC.Models.ViewModels;
+
+public class LogoutContribuyenteViewModel
+{
+    public Contribuyente Contribuyente { get; set; }
+}
